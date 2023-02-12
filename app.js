50b839b0b96fbffd6eb53cf0fb7bef3c96c9bbaa
serenity smith <serenitysmith24@gmail.com>
2023-02-12 13:21:01 -0500

    string methods

---
+++ app.js
@@ -0,0 +1,55 @@
+
+
+var name = "serenity";
+// makes my name uppercase
+var capName = name.toUpperCase();
+//console.log(capName)
+// makes my name lowercase
+var lowerCaseName = capName.toLowerCase()
+//console.log(lowerCaseName)
+
+var combinedName = capName + lowerCaseName
+// combined upper and lowercase name
+//console.log(combinedName)
+
+var greeting = "Hello Bitches"
+var index = Math.floor(greeting.length / 2);
+//console.log(index)
+
+var firstHalf = greeting.slice(0,5);
+//console.log(firstHalf)
+
+var str = "serenity is cool"
+
+var newString = "serenity is cool".slice(0,8)
+var firstHalf = newString.toUpperCase();
+var lowerName = "serenity is cool".slice(8)
+var secondHalf = lowerName.toLowerCase();
+//console.log(secondHalf)
+
+
+var comboName = firstHalf + secondHalf
+console.log(comboName)
+
+
+
+// var str_length = str.length;
+// var partA = '';
+// var partB = '';
+// for (var i=0 ; i < Math.ceil (str_length/2) ; i++) {
+// partA+=string[i];
+// }
+// for (var i=Math.ceil(str_length/2); i<str_length ; i++) {
+//     partB+=str_length[i];
+// }
+// var newPartA = partA.toUpperCase();
+//var changedStr = newPartA + partB
+//var slc = Math.floor(str.length / 2)
+//console.log(slc)
+
+//console.log(changedStr)
+
+
+
+
+
